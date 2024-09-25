5b6a9698bfa2c7b91be50bb4aace9bbe1cd887f3
digitalogiesather <attherzahid786@gmail.com>
2024-09-25 22:13:37 +0500

    updating home page.

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($){
+    $('.single-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true
+      });
+});
