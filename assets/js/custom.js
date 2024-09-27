f8dcf2746b8519b618d632b430f56b81d4586605
Attherzahid <attherzahid786@gmail.com>
2024-09-27 09:50:30 +0500

    frontpage

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,37 @@
-jQuery(document).ready(function($){
-    $('.single-slider').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        autoplay: true
-      });
+jQuery(document).ready(function ($) {
+  $('.single-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true
+  });
+
+  $('.multiple-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 });
